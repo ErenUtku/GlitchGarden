91ff1c1e91f454a12f17bdcada091b84771b418d
ErenUtku <61837087+ErenUtku@users.noreply.github.com>
2020-12-05 20:43:28 +0300

    Patch-1
    -Added Load Screen
    -Added Game Menu

---
+++ Assets/Sripts/LoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    Scene scene;
+    int CurrentSceneIndex;
+    float wait = 3.5f;
+    void Start()
+    {
+        CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (CurrentSceneIndex == 0)
+        {
+            StartCoroutine(LoadCoroutine());
+        }
+    }
+    IEnumerator LoadCoroutine()
+    {
+        yield return new WaitForSeconds(wait);
+        LoadNextScene();
+        
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+}
